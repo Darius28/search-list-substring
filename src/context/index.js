bab6dc87e9506bbc35aa7f35bcfa084eb38c7e5b
Darius28 <likith.dm.28@gmail.com>
2021-10-24 19:01:26 +0530

    input lowercase done

--- src/context/index.js
+++ src/context/index.js
@@ -1,18 +1,18 @@
-import React, { useReducer, createContext } from "react";
+import React, { useReducer, createContext } from 'react';
 
 const initialState = {
   sTerm: null,
   LIST_OF_ITEMS: [
-    { id: 1, name: "Tea" },
-    { id: 2, name: "Ice Tea" },
-    { id: 3, name: "Coffee" },
-    { id: 4, name: "Cold Coffee" },
-    { id: 5, name: "Ice Cream" },
-    { id: 6, name: "Ice Cube" },
-    { id: 7, name: "Sandwitch" },
-    { id: 8, name: "Pizza" },
-    { id: 9, name: "Burger" },
-    { id: 10, name: "Milkshakes" },
+    { id: 1, name: 'Tea' },
+    { id: 2, name: 'Ice Tea' },
+    { id: 3, name: 'Coffee' },
+    { id: 4, name: 'Cold Coffee' },
+    { id: 5, name: 'Ice Cream' },
+    { id: 6, name: 'Ice Cube' },
+    { id: 7, name: 'Sandwitch' },
+    { id: 8, name: 'Pizza' },
+    { id: 9, name: 'Burger' },
+    { id: 10, name: 'Milkshakes' },
   ],
   matchingItems: [],
 };
@@ -21,15 +21,20 @@ const Context = createContext();
 
 const rootReducer = (state, action) => {
   switch (action.type) {
-    case "USER_ENTRY": {
+    case 'USER_ENTRY': {
       const searchTerm = action.payload;
       let matchingItems = [];
       state.LIST_OF_ITEMS.forEach((value) => {
-        if (value.name.replace(/ +/g, "").toLowerCase().includes(searchTerm)) {
+        if (
+          value.name
+            .replace(/ +/g, '')
+            .toLowerCase()
+            .includes(searchTerm.toLowerCase())
+        ) {
           matchingItems.push(value);
         }
       });
-      console.log("mI: ", matchingItems);
+      console.log('mI: ', matchingItems);
       return { ...state, sTerm: action.payload, matchingItems };
     }
   }
